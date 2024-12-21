95c2b43f2ad1d9c3b114b1b1bda7fa213ff5446d
saajeapp <audreymaelys.cerrone@gmail.com>
2024-12-21 19:51:29 +0100

    update browserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/faq" element={<Faq />} />
-      {/* <Route path="/articles/:slug" element={<BlogPost />} />{" "} */}
       <Route path="/articles-recents" element={<BlogHome />} />
       <Route path="/les-regles-de-l-art-du-dating" element={<Blog1 />} />
       <Route
@@ -33,7 +32,6 @@ function App() {
         path="/nos-conseils-d-experts-pour-un-rendez-vous-amoureux-reussi"
         element={<Blog4 />}
       />
-      {/* Route dynamique */}
       <Route path="/plus" element={<Plus />} />
       <Route path="/preinscription" element={<Preinscription />} />
       <Route path="/conseil" element={<Conseil />} />
@@ -41,7 +39,7 @@ function App() {
       <Route path="/condition-utilisation" element={<ConditionUtilisation />} />
       <Route path="/confidentialite" element={<Confidentialite />} />
       <Route path="/politique-cookie" element={<PolitiqueCookie />} />
-      <Route path="*" element={<Home />} />{" "}
+      <Route path="*" exact={true} element={<Home />} />{" "}
       {/* Redirige vers la page d'accueil */}
     </Routes>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App /> {/* render the Preinscription component */}
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
